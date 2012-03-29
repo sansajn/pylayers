c89ff6505447b728fa233168a7daaf217a3f6c85
Adam Hlavatovic <adam.hlavatovic@gmail.com>
2012-03-29 14:45:07 +0200

    + working zoom + working tile cacheing

--- tiles.py
+++ tiles.py
@@ -31,8 +31,7 @@ class Form(QtGui.QMainWindow):
 		self.connect(self.network, QtCore.SIGNAL(
 			'finished(QNetworkReply *)'),	self.tile_reply_event)
 		self.disk_cache = QtNetwork.QNetworkDiskCache()
-		self.disk_cache.setCacheDirectory(os.path.join(temp_directory(), 
-			'tiles'))
+		self.disk_cache.setCacheDirectory(os.path.join(temp_directory(), 'tiles'))
 		self.network.setCache(self.disk_cache)
 	
 	def paintEvent(self, e):
@@ -50,30 +49,37 @@ class Form(QtGui.QMainWindow):
 		print 'paint event: %f' % (dt, )
 
 	def draw_map(self, qp):
-		x,y = self.visible_tiles()
-		for i in range(y[0], y[1]+1):
-			for j in range(x[0], x[1]+1):
+		print '#draw_map()'
+		y,x = self.visible_tiles()
+		print y,x
+		for i in range(y[0], y[1]):
+			for j in range(x[0], x[1]):
 				tile = self.load_tile_from_image_cache(j, i)
 				if tile == None:
 					tile = self.load_tile_from_disk_cache(j, i)
 					if tile:
 						self.insert_tile_to_image_cache(tile, j, i)
 				if tile:
-					self.draw_tile(tile, j, i. qp)
+					self.draw_tile(tile, j, i, qp)
+		print 'x0:%d, y0:%d' % self.view_offset
+		print 'visible tiles:%s' % ((y,x), )
 
-	def draw_tile(tile, x, y, qp):
-		x0,y0 = self.view_offset()
+	def draw_tile(self, tile, x, y, qp):
+		x0,y0 = self.view_offset
 		qp.drawImage(QtCore.QPoint(x*256+x0, y*256+y0), tile)
 
 	def load_tile_from_image_cache(self, x, y):
-		return self.image_cache.get(y).get(x)
+		return self.find_in_image_cache(x, y, self.zoom)
 
 	def load_tile_from_disk_cache(self, x, y):
-		url = QtCore.QUrl('http://tile.openstreetmap.org/%d/%d/%d.png' % (
-			self.zoom, x, y))
-		cache_item = self.disk_cache(url)
+		url = QtCore.QUrl(self.construct_tile_url(x, y, self.zoom))
+		cache_item = self.disk_cache.data(url)
 		if cache_item:
-			return QtCore.QImage(cache_item, 'PNG')
+			tile = QtGui.QImage()
+			tile.load(cache_item, 'PNG')
+			print 'tile z:%d, x:%d, y:%d loaded from disk cache' % (
+				self.zoom, x, y)
+			return tile
 		else:
 			return None
 
@@ -84,13 +90,15 @@ class Form(QtGui.QMainWindow):
 			self.image_cache[y] = {x:tile}
 
 	def map_changed(self):
+		print '#map_changed()'
 		tiles = self.tiles_not_in_cache(self.visible_tiles())
+		print '  tiles not in cache: %s' % (tiles, )
 		for t in tiles:
-			self.tile_request(t[0], t[1], self.zoom)		
+			self.tile_request(t[1], t[0], self.zoom)
 		self.standard_update()
 
 	def tile_request(self, x, y, z):
-		if (x,y,z) in self.requested_set:
+		if (x, y, z) in self.requested_tiles:
 			return
 		url = QtCore.QUrl(self.construct_tile_url(x, y, z))
 		request = QtNetwork.QNetworkRequest()		
@@ -98,16 +106,32 @@ class Form(QtGui.QMainWindow):
 		request.setRawHeader('User-Agent', 'PySlippyMap client')
 		request.setAttribute(QtNetwork.QNetworkRequest.CacheLoadControlAttribute,
 			QtNetwork.QNetworkRequest.PreferCache)
-		request.setAttribute(QNetworkRequest.HttpPipeliningAllowedAttribute,	True)
-		request.setAttribute((x, y, z))
+		request.setAttribute(
+			QtNetwork.QNetworkRequest.HttpPipeliningAllowedAttribute, True)
+		request.setAttribute(QtNetwork.QNetworkRequest.User, (x, y, z))
 		self.requested_tiles.add((x, y, z))
 		self.network.get(request)
+		print '\n#tile_request()'
+		print 'network request for tile z:%d, x:%d, y:%d' % (z, x, y)
 
 	def tile_reply_event(self, reply):
-		self.disk_cache.insert(reply)
-		id = reply.attribute(QtNetwor.QNetworkRequest.User).toPyObject()
-		self.requested_tiles.remove(id)
+		if reply.error():
+			print '\n#tile_reply_event(): error=%d' % (reply.error(),)
+			return
+
+		tile_id = reply.request().attribute(
+			QtNetwork.QNetworkRequest.User).toPyObject()
+
+		source = 'network'
+		from_cache = reply.attribute(
+			QtNetwork.QNetworkRequest.SourceIsFromCacheAttribute).toBool()
+		if from_cache:
+			source = 'disk cache'
+		print '\n#tile_reply_event()'
+		print '  tile %s received from %s' % (tile_id, source)
+
 		self.map_changed()
+
 
 	def standard_update(self):
 		self.paint_reason.reason = Form.paint_reason.STANDARD_REDRAW
@@ -123,47 +147,63 @@ class Form(QtGui.QMainWindow):
 		N = M = 2**self.zoom
 		w_t = h_t = 256
 		rc_view = ((0, h), (w, 0)) 
-		rc_map = ((x0, N*h_t+y0), (M*w_t+x0)) 
+		rc_map = ((x0, N*h_t+y0), (M*w_t+x0, y0)) 
 
 		if is_intersection_empty(rc_map, rc_view):
 			return []
 		else:
 			if x0 < 0:
-				n = ( abs(x0)/w_t, math.min((abs(x0)+w)/w_t, N) )
+				n = ( abs(x0)/w_t, min(int(math.ceil((abs(x0)+w)/float(w_t))), N-1)+1 )
 			else:
-				n = ( 0, math.min((w-x0)/w_t), N )
+				n = ( 0, min(int(math.ceil((w-x0)/float(w_t))), N-1)+1 )
 			if y0 < 0:
-				m = ( abs(y0)/h_t, min((abs(y0)+h)/h_t, M) )
+				m = ( abs(y0)/h_t, min(int(math.ceil((abs(y0)+h)/float(h_t))), M-1)+1 )
 			else:
-				m = ( 0, math.min((h-y0)/h_t, M) )
+				m = ( 0, min(int(math.ceil((h-y0)/float(h_t))), M-1)+1 )
 		return (m, n)
 
-	def tiles_not_in_cache(self, visible_tiles):
+	def tiles_not_in_cache(self, visible_tile_range):
 		tiles = []
-		for t in visible_tiles:
-			if self.find_in_image_cache(t[0], t[1], self.zoom) == None:
-				if self.find_in_disk_cache(t[0], t[1], self.zoom) == None:
-					tiles.append(t)							
+		visible_tile_list = self.tile_list_from_range(visible_tile_range)
+		for t in visible_tile_list:
+			if self.find_in_image_cache(t[1], t[0], self.zoom) == None:
+				if self.find_in_disk_cache(t[1], t[0], self.zoom) == None:
+					tiles.append(t)
+		return tiles
+
+	def tile_list_from_range(self, tile_range):
+		tiles = []
+		print '\n#tile_list_from_range(): len(tile_range)=%d' % (
+			len(tile_range), )
+		m,n = tile_range
+		for y in range(m[0], m[1]):
+			for x in range(n[0], n[1]):
+				tiles.append((y, x))
 		return tiles
 
 	def find_in_image_cache(self, x, y, z):
 		if z != self.zoom:
 			return None
-		row = self.get(y)
-		if row != None:
+		row = self.image_cache.get(y)
+		if row:
 			return row.get(x)
 		else:
 			return None
 
 	def find_in_disk_cache(self, x, y, z):
 		url = QtCore.QUrl(self.construct_tile_url(x, y, z))
-		return self.disk_cache(url)
+		return self.disk_cache.data(url)
 
 	def construct_tile_url(self, x, y, z):		
 		return 'http://tile.openstreetmap.org/%d/%d/%d.png' % (z, x, y)
 
+	def clear_image_cache(self):
+		self.image_cache = {}
+
 	def zoom_event(self, step):
 		self.zoom = max(self.zoom+step, 0) % 16
+		print '\n#zoom_event(): zoom event, zoom:%d' % (self.zoom, )
+		self.clear_image_cache()
 		self.map_changed()
 
 	def pan_event(self, diff):
@@ -183,7 +223,11 @@ class Form(QtGui.QMainWindow):
 	def mouseMoveEvent(self, e):
 		diff = (e.x() - self.click_pos[0], e.y() - self.click_pos[1])
 		self.click_pos = (e.x(), e.y())
-		pan_event(diff)
+		self.pan_event(diff)
+
+	def resizeEvent(self, e):
+		print '#resizeEvent()'
+		self.map_changed()
 
 
 def is_intersection_empty(r1, r2):
